0248b02446897201f78a7bcbd1eeac4ac111db3c
Prigless <89812657+Prigless@users.noreply.github.com>
2022-08-01 23:48:41 +0200

    Red_Light_Green_Light.py

--- README.md
+++ README.md
@@ -5,8 +5,18 @@ Cheats for Kukoro: Stream chat games
 * Colorblind Floor
 * 1, 2, 3... STOP! (Red Light, Green Light)
 
+<br>
+
+
 ### How To Use Colorblind_Floor.py:
 1. Open Twitch Stream In Theater Mode
-2. Printscreen The Window
+2. Printscreen The Window (alt+printscreen) (1920, 1040)
 3. Run Colorblind_Floor.py
 4. Use OUTPUT_IMAGE.png To Find The Correct Floor!
+
+### How To Use Red_Light_Green_Light.py:
+1. Go To https://twitchtokengenerator.com/ And Select "Bot Chat Token"
+2. Copy ACCESS TOKEN
+3. Paste Token To config.txt
+4. Supply config.txt With Rest
+5. Run Red_Light_Green_Light.py
---
+++ Red_Light_Green_Light/Red_Light_Green_Light.py
@@ -0,0 +1,40 @@
+﻿from twitchio.ext import commands
+import time
+import random
+
+with open('config.txt', 'r') as f:
+    CONFIG=f.readlines()
+
+CHANNEL=CONFIG[0][9:-2]
+ACCESS_TOKEN=CONFIG[1][14:-1]
+
+print(CHANNEL)
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(token=ACCESS_TOKEN, prefix='', initial_channels=[CHANNEL])
+
+    async def event_ready(self):
+        print(f'Logged in as | {self.nick}')
+
+bot = Bot()
+
+@bot.event()
+async def event_message(msg):
+    if msg.echo:
+        return
+    print(msg.author.name+': '+str(msg.content))
+    if msg.author.name == CHANNEL:
+        time.sleep(round(random.uniform(0.1, 0.4), 10))
+        if msg.content == '☺ACTION [KUKORO] <<< YOU CAN MOVE! >>>☺':
+            chan = bot.get_channel(CHANNEL)
+            print('chan.send("!go")\n'*10)
+            await chan.send("!go")
+        elif msg.content == '☺ACTION [KUKORO] <<< STOP! >>>☺':
+            chan = bot.get_channel(CHANNEL)
+            print('chan.send("!stop")\n'*10)
+            await chan.send("!stop")
+    await bot.handle_commands(msg)
+
+bot.run()
+
+
